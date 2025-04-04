aba59552f924207c8c7182458bce49cbba541c42
Brin Marx <brin.marx@wostmann.com>
2025-04-03 23:29:06 -0800

    Added map

--- JEDCMobilityManager.Web/wwwroot/js/site.js
+++ JEDCMobilityManager.Web/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿
+﻿[HTMLCollection.prototype, NodeList.prototype].forEach(a => Object.defineProperties(a, {
+    toArray: { value: Array.prototype.slice },
+    forEach: { value: Array.prototype.forEach },
+    map: { value: Array.prototype.map },
+    reduce: { value: Array.prototype.reduce },
+    filter: { value: Array.prototype.filter },
+    group: { value: Array.prototype.group },
+    every: { value: Array.prototype.every },
+    some: { value: Array.prototype.some },
+    find: { value: Array.prototype.find }
+}));
+
