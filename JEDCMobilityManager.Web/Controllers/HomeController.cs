da829e92941c7bde9158092abbe60cae431add92
Brin Marx <brin.marx@wostmann.com>
2025-04-15 00:09:41 -0800

    Added features endpoint

--- JEDCMobilityManager.Web/Controllers/HomeController.cs
+++ JEDCMobilityManager.Web/Controllers/HomeController.cs
@@ -9,5 +9,32 @@ namespace JEDCMobilityManager.Web.Controllers
         {
             return View(Area.GetAll(Connection));
         }
+
+        public string Features(string name)
+        {
+            return string.Format("{{ \"type\": \"FeatureCollection\", \"features\": [{0}] }}", string.Join(',', Area.GetAll(Connection, FeatureQuery).Select(feature => $@"{{
+                ""type"": ""Feature"",
+                ""properties"": {{
+                    ""name"": ""{feature.Name}"",
+                    ""visitors"": {feature.OtherValues["Visitors"]},
+                    ""residents"": {feature.OtherValues["Residents"]}
+                }},
+                ""geometry"": {feature.GeoJson}
+            }}")));
+        }
+
+        private const string FeatureQuery = @"
+            SELECT A.[Id], A.[Name], A.[Shape].STAsText(), V.[0] AS [Visitors], V.[1] AS [Residents]
+            FROM (
+	            SELECT PA.[AreaId], P.[IsResident], COUNT(*) AS [Count]
+	            FROM [dbo].[PersonArea] PA
+	            JOIN [dbo].[Person] P ON P.[Id] = PA.[PersonId]
+	            GROUP BY PA.[AreaId], P.[IsResident]
+            ) B
+            PIVOT (
+	            SUM(B.[Count])
+	            FOR B.[IsResident] IN ([0], [1])
+            ) V
+            RIGHT JOIN [dbo].[Area] A ON A.[Id] = V.[AreaId]";
     }
 }
